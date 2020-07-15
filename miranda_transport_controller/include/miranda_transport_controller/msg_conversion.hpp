3b91768b61b979b661c656290b8a61bce498b4e1
Heinrich <Heinrich>
2020-07-15 12:22:05 +0200

    Added documentation

--- miranda_transport_controller/include/miranda_transport_controller/msg_conversion.hpp
+++ miranda_transport_controller/include/miranda_transport_controller/msg_conversion.hpp
@@ -7,39 +7,74 @@
 #include<geometry_msgs/Pose.h>
 #include<std_msgs/Float64.h>
 
-void convertMsg(std_msgs::Float64 &msg,Eigen::Quaterniond &quat)
+/**
+ * @brief Convert an float64 z-axis angle to an eigen quaternion
+ * 
+ * @param msg Float64 ros message with z-axis angle
+ * @param quat Eigen quaterniond the quaternion is stored in
+ */
+void convertMsg(Eigen::Quaterniond &quat,std_msgs::Float64 &msg)
 {
    quat=    Eigen::AngleAxisd(0.0, Eigen::Vector3d::UnitX())
             * Eigen::AngleAxisd(0.0,  Eigen::Vector3d::UnitY())
             * Eigen::AngleAxisd(msg.data, Eigen::Vector3d::UnitZ());
 }
 
-void convertMsg(geometry_msgs::Transform &msg,Eigen::Quaterniond &quat)
+/**
+ * @brief Convert Orientation of a Transform geometry message to an eigen quaternion
+ * 
+ * @param msg Message that holds the transform
+ * @param quat Quaternion the eigen quaternion is stored in
+ */
+void convertMsg(Eigen::Quaterniond &quat,geometry_msgs::Transform &msg)
 {
     Eigen::Vector4d vec;
     vec<<msg.rotation.w, msg.rotation.x, msg.rotation.y, msg.rotation.z;
     quat=Eigen::Quaterniond(vec);
 }
-void convertMsg(geometry_msgs::TransformStamped &msg,Eigen::Quaterniond &quat)
+
+/**
+ * @brief Convert an StampedTransform geometry message to an eigen quaternion
+ * 
+ * @param msg Message that holds the stamped transform
+ * @param quat Quternion the eigen quaternion is stored in
+ */
+void convertMsg(Eigen::Quaterniond &quat,geometry_msgs::TransformStamped &msg)
 {
-    convertMsg(msg.transform,quat);
+    convertMsg(quat,msg.transform);
 }
 
-void convertMsg(geometry_msgs::Pose &msg,Eigen::Quaterniond &quat)
+/**
+ * @brief Convert an Pose geometry message to an eigen quaternion
+ * 
+ * @param msg Message that holds the pose
+ * @param quat Quternion the eigen quaternion is stored in
+ */
+void convertMsg(Eigen::Quaterniond &quat,geometry_msgs::Pose &msg)
 {
     Eigen::Vector4d vec;
     vec<<msg.orientation.w, msg.orientation.x, msg.orientation.y, msg.orientation.z;
     quat=Eigen::Quaterniond(vec);
 }
-
-void convertMsg(geometry_msgs::PoseStamped &msg,Eigen::Quaterniond &quat)
+/**
+ * @brief Convert an stamped pose geometry message to an eigen quaternion
+ * 
+ * @param msg Message that holds the stamped pose
+ * @param quat Quternion the eigen quaternion is stored in
+ */
+void convertMsg(Eigen::Quaterniond &quat,geometry_msgs::PoseStamped &msg)
 {
-   convertMsg(msg.pose,quat);
+   convertMsg(quat,msg.pose);
 }
 
 
 
-
+/**
+ * @brief Convert an pose geometry message to an Eigen pose vector (x, y, z, w, x, y, z)
+ * 
+ * @param msg Message that holds the pose
+ * @param pose Matrix (Vector) the eigen pose vector is stored in
+ */
 void convertMsg(Eigen::Matrix<double,7,1> &pose,geometry_msgs::Pose &msg)
 {
     pose<<  msg.position.x,
@@ -52,11 +87,23 @@ void convertMsg(Eigen::Matrix<double,7,1> &pose,geometry_msgs::Pose &msg)
    ;
 }
 
+/**
+ * @brief Convert an stamepd pose geometry message to an Eigen pose vector (x, y, z, w, x, y, z)
+ * 
+ * @param msg Message that holds the pose
+ * @param pose Matrix (Vector) the eigen pose vector is stored in
+ */
 void convertMsg(Eigen::Matrix<double,7,1> &pose,geometry_msgs::PoseStamped &msg)
 {
     convertMsg(pose,msg.pose);
 }
 
+/**
+ * @brief Convert  an Eigen pose vector (x, y, z, w, x, y, z) to a pose geometry message
+ * 
+ * @param msg Message that the pose is stored in
+ * @param pose Matrix (Vector) that holds the pose
+ */
 void convertMsg(geometry_msgs::Pose &msg,Eigen::Matrix<double,7,1> &pose)
 {
     msg.position.x=pose(0);
@@ -68,11 +115,23 @@ void convertMsg(geometry_msgs::Pose &msg,Eigen::Matrix<double,7,1> &pose)
     msg.orientation.z=pose(6);
 }
 
+/**
+ * @brief Convert  an Eigen pose vector (x, y, z, w, x, y, z) to a stamepd pose geometry message
+ * 
+ * @param msg Message that the pose is stored in
+ * @param pose Matrix (Vector) that holds the pose
+ */
 void convertMsg(geometry_msgs::PoseStamped &msg,Eigen::Matrix<double,7,1> &pose)
 {
     convertMsg(msg.pose,pose);
 }
 
+/**
+ * @brief Convert  an Eigen pose vector (x, y, z, w, x, y, z) to a transform geometry message
+ * 
+ * @param msg Message that the pose is stored in
+ * @param pose Matrix (Vector) that holds the pose
+ */
 void convertMsg(geometry_msgs::Pose &pose,geometry_msgs::Transform &transform)
 {
     pose.position.x=transform.translation.x;
@@ -84,12 +143,25 @@ void convertMsg(geometry_msgs::Pose &pose,geometry_msgs::Transform &transform)
     pose.orientation.z=transform.rotation.z;
     pose.orientation.w=transform.rotation.w;
 }
+
+/**
+ * @brief Convert a stamped transform geometry message to a stamped pose geometry message
+ * 
+ * @param pose Stamped pose the pose is stored in
+ * @param transformStamped The geometry msgs that holds the transform
+ */
 void convertMsg(geometry_msgs::PoseStamped &pose,geometry_msgs::TransformStamped &transformStamped)
 {
    pose.header=transformStamped.header;
    convertMsg(pose.pose,transformStamped.transform);
 }
 
+/**
+ * @brief Convert a geometry pos messag to a geometry transfomr message
+ * 
+ * @param transform Message the transform is stored in
+ * @param pose Message that holds the pose
+ */
 void convertMsg(geometry_msgs::Transform &transform,geometry_msgs::Pose &pose)
 {
     transform.translation.x=pose.position.x;
@@ -104,6 +176,12 @@ void convertMsg(geometry_msgs::Transform &transform,geometry_msgs::Pose &pose)
     
 }
 
+/**
+ * @brief Convert a geometry stamped pose message to a stamped transform geometry message
+ * 
+ * @param transform Message the stamped transform is stored in
+ * @param pose Message that holds the pose
+ */
 void convertMsg(geometry_msgs::TransformStamped &transform,geometry_msgs::PoseStamped &pose)
 {
     transform.header=pose.header;
