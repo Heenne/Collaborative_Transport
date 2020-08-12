6e08d1ce6d332c765b0b22fe4d01359d1f2689b1
Heinrich <Heinrich>
2020-08-12 14:50:17 +0200

    Fixed string bug in exception

--- multi_robot_controller/include/multi_robot_controller/necessary_param_exeption.hpp
+++ multi_robot_controller/include/multi_robot_controller/necessary_param_exeption.hpp
@@ -2,23 +2,13 @@
 #include<exception>
 #include<string>
 
-class NecessaryParamException:public std::exception
+class NecessaryParamException:public std::runtime_error
 {
     public:
-        NecessaryParamException()
+        NecessaryParamException():runtime_error("Not specified")
         {
-            this->param_name_="Not specified";
         }   
-        NecessaryParamException(std::string param_name_resolved)
-        {
-            this->param_name_=param_name_resolved;
+        NecessaryParamException(std::string param_name_resolved):runtime_error(param_name_resolved)
+        {            
         }
-        virtual const char* what() const throw() override
-        {
-            std::stringstream str;
-            str<<"Could not find parameter with name: "<<this->param_name_;
-            return str.str().c_str();
-        }
-    private:
-        std::string param_name_;
 };
