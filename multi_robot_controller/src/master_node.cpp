0648581da044cc8cce247cb4f93fd992ff435a15
Heinrich <Heinrich>
2020-08-12 14:50:39 +0200

    Switched launch files to new controller implem

--- multi_robot_controller/src/master_node.cpp
+++ multi_robot_controller/src/master_node.cpp
@@ -4,6 +4,8 @@ int main(int argc, char **argv)
 {
     ros::init(argc,argv,"master");
     ros::NodeHandle nh;
+    ros::NodeHandle priv("~");
+    priv.setParam("reference",std::vector<double>{0.0,0.0,0.0});
     LyapunovController master(nh);
     ros::spin();
 }
