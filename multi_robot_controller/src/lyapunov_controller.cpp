e35c1579194bc9a2ef1135b749e3a56ea47e5316
Heinrich <Heinrich>
2020-08-17 11:54:12 +0200

    Implemented dynamic reconfigure for lyapunov param

--- multi_robot_controller/src/lyapunov_controller.cpp
+++ multi_robot_controller/src/lyapunov_controller.cpp
@@ -15,7 +15,8 @@ LyapunovController::LyapunovController(ros::NodeHandle &nh):Controller(nh)
     else
     {
         this->parameter_=LyapunovParameter(lyapunov);
-    }          
+    }     
+    this->server_.setCallback(boost::bind(&LyapunovController::dynConfigcallback,this,_1,_2));     
 
 }
 
@@ -24,11 +25,11 @@ Controller::ControlVector LyapunovController::calcControl(State current_state ,S
     double omega=target_state.ang_vel.z();
     double v=sqrt(pow(target_state.lin_vel.x(),2)+pow(target_state.lin_vel.y(),2)); 
     
-    tf::Transform control_dif_=current_state.pose.inverseTimes(target_state.pose);
+    tf::Transform control_dif=current_state.pose.inverseTimes(target_state.pose);
     
-    double x=control_dif_.getOrigin().getX();
-    double y=control_dif_.getOrigin().getY();
-    double phi=tf::getYaw(control_dif_.getRotation());
+    double x=control_dif.getOrigin().getX();
+    double y=control_dif.getOrigin().getY();
+    double phi=tf::getYaw(control_dif.getRotation());
 
 
     ControlVector output;
@@ -36,4 +37,9 @@ Controller::ControlVector LyapunovController::calcControl(State current_state ,S
     output.omega=omega+this->parameter_.ky*v*y+this->parameter_.kphi*sin(phi);
 
     return output;      
+}
+
+void LyapunovController::dynConfigcallback(multi_robot_controller::LyapunovConfig &config, uint32_t level) {
+    ROS_INFO("Reconfigure Request: %lf %lf %lf",config.kx,config.ky,config.kphi);
+    this->parameter_=LyapunovParameter(config.kx,config.ky,config.kphi);
 }
