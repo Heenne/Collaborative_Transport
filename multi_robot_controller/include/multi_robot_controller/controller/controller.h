88ff0be01617f1f2bec5efc0b6046159307d4d96
Heinrich <Heinrich>
2020-08-24 11:41:17 +0200

    Added doc

--- multi_robot_controller/include/multi_robot_controller/controller/controller.h
+++ multi_robot_controller/include/multi_robot_controller/controller/controller.h
@@ -17,37 +17,88 @@
 class Controller
 {
     public:
+        /**
+         * @brief Construct a new Controller object
+         * 
+         */
         Controller();
+
+        /**
+         * @brief Construct a new Controller object with given Nodehandle
+         * 
+         * @param nh Nodehandle for resolving topics
+         */
         
         Controller(ros::NodeHandle &nh);
 
+        /**
+         * @brief Destroy the Controller object
+         * 
+         */
         ~Controller(); 
 
+        /**
+         * @brief Defines the controllers output
+         * 
+         */
         struct ControlVector
         {
-            double v;                   ///<Linear velocity
-            double omega;               ///<Angular velocity
+            double v;                   ///<Linear velocity of plattform
+            double omega;               ///<Angular velocity of plattform
             /**
              * @brief Construct a new Control Vector object with default parameters
              * 
              */
             ControlVector(){v=0.0;omega=0.0;}
+            /**
+             * @brief Construct a new Control Vector object
+             * 
+             * @param v Linear velocity of plattform
+             * @param omega Angular velocity of plattform
+             */
             ControlVector(double v, double omega)
             {
                 this->v=v;
                 this->omega=omega;
             }
         };
+        /**
+         * @brief Defines the state representations within the Controller
+         * 
+         */
         struct State
         {
-            tf::Pose pose;
-            tf::Vector3 lin_vel;
-            tf::Vector3 ang_vel;
+            tf::Pose pose;  //< Pose of the mobile robot in space
+            tf::Vector3 lin_vel;    //<Linear velocity of the robot in space
+            tf::Vector3 ang_vel;    //<Angular velocity of the robot in space
+            
+            /**
+             * @brief Construct a new State object
+             * 
+             */
             State();
+            /**
+             * @brief Construct a new State object
+             * 
+             * @param pose Pose of the mobile robot in space
+             * @param lin_vel Linear velocity of the robot in space
+             * @param ang_vel Angular velocity of the robot in space
+             */
             State(tf::Pose pose,tf::Vector3 lin_vel,tf::Vector3 ang_vel);      
         };
     protected:
+        /**
+         * @brief Control law fucntion. Determines the control vector by a given current and target state. Is implemented in derived Classes
+         * 
+         * @param current_state Current state of the controlled robot.
+         * @param target_state State of the mobile robot to be reached.
+         * @return ControlVector Commands to gain the target state from current state
+         */
         virtual ControlVector calcControl(State current_state,State target_state)=0;
+        /**
+         * @brief Publishes the meta data of the controller. Thes are e.g. the current and target states and the control differences.
+         * 
+         */
         virtual void publishMetaData();
         ros::Publisher meta_;
     
