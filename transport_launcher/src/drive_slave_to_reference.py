28d83263e50fb6af441d144fa3ebcf9bd2941c68
Heinrich <Heinrich>
2020-08-24 09:08:20 +0200

    Fixed drive_to reference bugs

--- transport_launcher/src/drive_slave_to_reference.py
+++ transport_launcher/src/drive_slave_to_reference.py
@@ -1,23 +1,48 @@
 #! /usr/bin/env python
-
+import json
 import rospy
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from geometry_msgs.msg import PoseStamped
 import tf2_geometry_msgs as tf2g
 from geometry_msgs.msg import TransformStamped
+import tf2_ros
+import numpy as np
 from std_srvs.srv import Empty
-
+from tf import transformations
+import tf
+import tf2_ros
 
 
 class SlaveReferencer:
-    def __init__(self,ref_dict,master):
+    def __init__(self):
         rospy.init_node('slave_referencer')
-        self.slave_names=ref_dict.keys()
-        self.reference_dict=ref_dict
-        self.slave_poses=dict((el,PoseStamped) for el in self.slave_names)    
-        
-        self.master_sub=rospy.Subscriber("master_pose",PoseStamped,self.__masterCallback__)
+        slaves_ns=rospy.get_param('~slave_namespaces')
+        self.master=rospy.get_param('~master_namespace')
+        if not slaves_ns or not self.master:
+            raise ValueError()
+        rospy.loginfo("Master")
+        rospy.loginfo(self.master)
+        rospy.loginfo("Slaves:")
+        rospy.loginfo(slaves_ns)
+
+        self.slaves=dict()
+        self.slave_poses=dict()
+        for slave in slaves_ns:
+            ref=rospy.get_param("/"+slave.strip("/")+"/reference")
+            ref_pose=PoseStamped()
+            ref_pose.pose.position.x=ref[0]
+            ref_pose.pose.position.y=ref[1]
+            ref_pose.pose.position.z=0.0
+            quat=transformations.quaternion_from_euler(0.0,0.0,ref[2])
+            ref_pose.pose.orientation.x=quat[0]
+            ref_pose.pose.orientation.y=quat[1]
+            ref_pose.pose.orientation.z=quat[2]
+            ref_pose.pose.orientation.w=quat[3]
+            self.slaves[slave]=ref_pose
+            self.slave_poses[slave]=PoseStamped()
+        rospy.loginfo("Slaves:")
+        rospy.loginfo(self.slaves)
         self.maser_pose=PoseStamped()
 
         self.set_state_srv=rospy.Service("toggle_state",Empty,self.__srvsetState__)
@@ -27,25 +52,22 @@ class SlaveReferencer:
         self.movement_counter=0
 
         self.move_base_dict=dict()
-        for name in self.slave_names:                
-               self.move_base_dict[name]=actionlib.SimpleActionClient(name+'/move_base',MoveBaseAction)
-    
-    def __masterCallback__(self,msg):
-        self.maser_pose=msg
+        for slave in self.slaves:                
+               self.move_base_dict[slave]=actionlib.SimpleActionClient(slave+'/move_base',MoveBaseAction)
 
     def __calcSlaves__(self):        
         master_pose=rospy.wait_for_message("/master_pose_stamped",PoseStamped)
-        for name in self.slave_names:
+        for slave in self.slaves:
+            print(self.slaves[slave])
             trafo=TransformStamped()
             trafo.transform.translation=master_pose.pose.position
             trafo.transform.rotation=master_pose.pose.orientation            
-            slave_pose=tf2g.do_transform_pose(self.reference_dict[name],trafo)
-            self.slave_poses[name]=slave_pose
-        
+            slave_pose=tf2g.do_transform_pose(self.slaves[slave],trafo)
+            self.slave_poses[slave]=slave_pose
     
     def __moveSlaves__(self):
-        for name in self.slave_names:            
-            self.__moveSlave__(name)
+        for slave in self.slaves:            
+            self.__moveSlave__(slave)
 
     def __moveSlave__(self,name):
         rospy.loginfo("Moving slave "+name+" to:")
@@ -80,12 +102,12 @@ class SlaveReferencer:
             pass
                         
         elif self.state==3:
-            if self.movement_counter==len(self.slave_names):
+            if self.movement_counter==len(self.slaves):
                 self.state=0
                 self.movement_counter=0
                 rospy.loginfo("3->0")
             else:
-               self.__moveSlave__(self.slave_names[self.movement_counter])
+               self.__moveSlave__(self.slaves.keys()[self.movement_counter])
                self.movement_counter=self.movement_counter+1
                self.state=2
                rospy.loginfo("3->2")
@@ -104,7 +126,7 @@ if __name__=="__main__":
     pose2.pose.orientation.w=1.0
     # names={"/mur/mir":pose1,"/miranda/mir":pose2}
     names={"/mur/mir":pose1}
-    ref=SlaveReferencer(names,"virtual_master")
+    ref=SlaveReferencer()
     while not rospy.is_shutdown():
         ref.run()
     
