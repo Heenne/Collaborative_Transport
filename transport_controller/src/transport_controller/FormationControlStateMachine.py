79aa05fe0f11bacb7d5d964070bda42e98d9b1b7
Heinrich <Heinrich>
2020-08-26 17:05:39 +0200

    Prepared Formation Control State machine

--- transport_controller/src/transport_controller/FormationControlStateMachine.py
+++ transport_controller/src/transport_controller/FormationControlStateMachine.py
@@ -1,2 +1,18 @@
 import smach
 import FormationControlStates as st
+
+class FormationControlStateMachine(smach.StateMachine): 
+    def __init__(self,slave_namespaces):
+        smach.StateMachine.__init__(self,   outcomes=['formation_control_done'])
+       
+        with self:      
+            smach.StateMachine.add("Idle",st.FormationControlIdleState(),
+                                    transitions={   'enable':EnableControl},
+                                                    'disable':'DisableControl',
+                                                    'stop':'formation_control_done'})
+                                                    
+            smach.StateMachine.add('EnableControl',st.FormationControlServiceState(slave_namespaces,"enable_controller"),
+                                    transitions={'called':'DisableControl'})
+
+            smach.StateMachine.add('DisableControl',st.FormationControlServiceState(slave_namespaces,"enable_controller"),
+                                    transitions={'called':'CalcPoses'})
--- transport_controller/src/transport_controller/FormationControlStates.py
+++ transport_controller/src/transport_controller/FormationControlStates.py
@@ -1,2 +1,29 @@
 import smach
 import FormationControlStates as st
+import rospy
+from std_srvs.srv   import Empty
+
+class FormationControlServiceState(smach.State):
+    def __init__(self,namespaces,service_name):
+        smach.State.__init__(outcomes=["called"])
+        self.clients=list()
+        self.service=rospy.Service("~"+service_name,Empty,self.__callback__)
+        for name in namespaces:
+            self.clients.append(rospy.ServiceProxy(name+'/'+service_name, Empty))
+        self.called=False
+
+    def execute(self,userdata):
+        while not self.called:
+            rospy.Rate(10).sleep()
+        for client in self.clients:
+            client.call(Empty())
+        self.called=False
+        return "called"
+    def __callback__(self,req):
+        self.called=True
+        return True
+
+class FormationControlIdleState(smach.State):
+    def __init__(self):
+        smach.State.__init__(outcomes=["enable",'disable','stop'])
+        pass
