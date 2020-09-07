ca0cabf8fda8749c16b89f86045311c31efc9131
Heinrich <Heinrich>
2020-09-07 12:46:30 +0200

    Added ps4 control for statemachine

---
+++ transport_controller/nodes/ps4_statemachine_controller.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import TwistStamped,Twist
+from transport_controller.PS4 import PlayStationStateMachineHandler
+
+if __name__=="__main__":
+    rospy.init_node("ps4_state_machine_controller")
+    handler=PlayStationStateMachineHandler(TwistStamped,["trigger","enable","disable","move","stop"])
+    handler.run()
